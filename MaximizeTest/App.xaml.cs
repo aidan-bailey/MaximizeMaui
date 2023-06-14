e85e9a531b43236cacd508ddeb0f2f84653a8b3c
Aidan Bailey <aidan.j.bailey@outlook.com>
2023-06-14 11:17:00 +0200

    Add project files.

---
+++ MaximizeTest/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MaximizeTest
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MaximizeTest/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace MaximizeTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            var window = Window.Handler.PlatformView as MauiWinUIWindow;
+            var handle = WinRT.Interop.WindowNative.GetWindowHandle(window);
+            var id = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(handle);
+            var appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(id);
+
+            switch (appWindow.Presenter)
+            {
+                case Microsoft.UI.Windowing.OverlappedPresenter overlappedPresenter:
+                    if (overlappedPresenter.State == Microsoft.UI.Windowing.OverlappedPresenterState.Maximized)
+                    {
+                        overlappedPresenter.SetBorderAndTitleBar(true, true);
+                        overlappedPresenter.Restore();
+                    }
+                    else
+                    {
+                        overlappedPresenter.SetBorderAndTitleBar(false, false);
+                        overlappedPresenter.Maximize();
+                    }
+
+                    break;
+            }
+        }
+
+    }
+}
